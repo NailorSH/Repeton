7a63f13a497d00f98bb6383f2263d628fa4bc3d0
Nail Shiyatov <nailshiyatov.04@mail.ru>
2023-12-24 21:27:08 +0300

    Add SearchBar

---
+++ app/src/main/java/com/nailorsh/repeton/ui/components/SearchBar.kt
@@ -0,0 +1,154 @@
+package com.nailorsh.repeton.ui.components
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.nailorsh.repeton.R
+import com.nailorsh.repeton.ui.theme.RepetonTheme
+
+@Composable
+fun SearchBarWithFilter(
+    @StringRes placeholder: Int,
+    @DrawableRes leadingIcon: Int,
+    @DrawableRes filterIcon: Int,
+    keyboardOptions: KeyboardOptions,
+    value: String,
+    onValueChanged: (String) -> Unit,
+    onFilterClicked: () -> Unit = { },
+    modifier: Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .border(
+                width = 2.dp,
+                color = Color.Black,
+                shape = RoundedCornerShape(size = 5.dp)
+            )
+            .fillMaxWidth()
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(size = 5.dp)
+            )
+            .padding(10.dp)
+    ) {
+        SearchBar(
+            modifier = Modifier.weight(1f),
+            placeholder = placeholder,
+            leadingIcon = leadingIcon,
+            keyboardOptions = keyboardOptions,
+            value = value,
+            onValueChanged = onValueChanged
+        )
+
+        Icon(
+            painter = painterResource(filterIcon),
+            contentDescription = null,
+            modifier = Modifier
+                .width(21.dp)
+                .height(17.dp)
+                .clickable { onFilterClicked() }
+        )
+    }
+}
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    @StringRes placeholder: Int,
+    @DrawableRes leadingIcon: Int,
+    keyboardOptions: KeyboardOptions,
+    value: String,
+    onValueChanged: (String) -> Unit,
+) {
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChanged,
+        keyboardOptions = keyboardOptions,
+        singleLine = true,
+        textStyle = TextStyle(
+            fontSize = 14.sp,
+            lineHeight = 17.sp,
+            fontWeight = FontWeight(400),
+            color = Color.Black
+        ),
+        modifier = modifier,
+        decorationBox = { innerTextField ->
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(
+                    painter = painterResource(leadingIcon),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(1.dp)
+                        .width(14.dp)
+                        .height(14.dp)
+                )
+
+                if (value.isEmpty()) {
+                    Text(
+                        text = stringResource(placeholder),
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                            lineHeight = 17.sp,
+                            fontWeight = FontWeight(400),
+                            color = Color(R.color.search_placeholder),
+                        ),
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+                innerTextField()
+            }
+        }
+    )
+}
+
+@Preview(
+    showSystemUi = true
+)
+@Composable
+fun SearchBarPreview() {
+    RepetonTheme {
+        SearchBarWithFilter(
+            placeholder = R.string.search_placeholder,
+            leadingIcon = R.drawable.search_icon,
+            filterIcon = R.drawable.filter_icon,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search
+            ),
+            onValueChanged = {},
+            modifier = Modifier,
+            value = ""
+        )
+    }
+}
--- app/src/main/java/com/nailorsh/repeton/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/nailorsh/repeton/ui/screens/SearchScreen.kt
@@ -4,25 +4,57 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.nailorsh.repeton.R
+import com.nailorsh.repeton.ui.components.SearchBarWithFilter
 import com.nailorsh.repeton.ui.theme.RepetonTheme
 
 @Composable
-fun SearchScreen() {
+fun SearchScreen(modifier: Modifier = Modifier) {
+    var query by remember { mutableStateOf("") }
 
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = modifier.padding(vertical = 15.dp),
+    ) {
+        Column (
+            modifier = Modifier.fillMaxWidth(0.9f)
+        ){
+            SearchBarWithFilter(
+                placeholder = R.string.search_placeholder,
+                leadingIcon = R.drawable.search_icon,
+                filterIcon = R.drawable.filter_icon,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Search
+                ),
+                value = query,
+                onValueChanged = { query = it},
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
 }
 
 @Composable
@@ -73,11 +105,6 @@ fun TutorCard(modifier: Modifier = Modifier) {
     }
 }
 
-@Composable
-fun SearchLine() {
-
-}
-
 @Preview
 @Composable
 fun TutorCardPreview() {
@@ -85,3 +112,4 @@ fun TutorCardPreview() {
         TutorCard()
     }
 }
+

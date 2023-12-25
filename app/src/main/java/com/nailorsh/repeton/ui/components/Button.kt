ed34ec14735c86605b23677ffc0238ef3be57759
Nail Shiyatov <nailshiyatov.04@mail.ru>
2023-12-25 18:59:04 +0300

    Add separation of reusable composable functions, add new elements

---
+++ app/src/main/java/com/nailorsh/repeton/ui/components/Button.kt
@@ -0,0 +1,64 @@
+package com.nailorsh.repeton.ui.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.nailorsh.repeton.R
+
+@Composable
+fun RepetonButton(
+    modifier: Modifier = Modifier,
+    @StringRes text: Int,
+    textColor: Color = Color.Black
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(5.dp, Alignment.CenterHorizontally),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .padding(horizontal = 5.dp, vertical = 10.dp)
+    ) {
+        Text(
+            text = stringResource(text),
+            style = TextStyle(
+                fontSize = 14.sp,
+                lineHeight = 32.sp,
+                fontWeight = FontWeight(600),
+                color = textColor
+            )
+        )
+    }
+}
+
+@Composable
+fun LikeButton(
+    modifier: Modifier = Modifier,
+    onButtonClicked: () -> Unit = {},
+    isLiked: Boolean
+) {
+    val iconRes = if (isLiked) R.drawable.liked_heart_icon else R.drawable.unliked_heart_icon
+
+    Icon(
+        imageVector = ImageVector.vectorResource(iconRes),
+        contentDescription = "",
+        tint = ImageVector.vectorResource(iconRes).tintColor,
+        modifier = modifier
+            .clickable {
+                onButtonClicked()
+            }
+    )
+}
--- app/src/main/java/com/nailorsh/repeton/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/nailorsh/repeton/ui/screens/SearchScreen.kt
@@ -1,8 +1,8 @@
 package com.nailorsh.repeton.ui.screens
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,21 +23,22 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.nailorsh.repeton.R
+import com.nailorsh.repeton.ui.components.ExpandableText
+import com.nailorsh.repeton.ui.components.IconWithText
+import com.nailorsh.repeton.ui.components.LikeButton
+import com.nailorsh.repeton.ui.components.RepetonButton
 import com.nailorsh.repeton.ui.components.SearchBarWithFilter
 import com.nailorsh.repeton.ui.theme.AmbientColor
 import com.nailorsh.repeton.ui.theme.RepetonTheme
@@ -53,7 +53,7 @@ fun SearchScreen(modifier: Modifier = Modifier) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top,
-        modifier = modifier.padding(vertical = 15.dp),
+        modifier = modifier.padding(top = 15.dp),
     ) {
         Column(
             verticalArrangement = Arrangement.spacedBy(18.dp, Alignment.Top),
@@ -93,6 +93,8 @@ fun TutorList(modifier: Modifier = Modifier) {
 
 @Composable
 fun TutorCard(modifier: Modifier = Modifier) {
+    var isLiked by remember { mutableStateOf(false) }
+
     Column(
         verticalArrangement = Arrangement.spacedBy(25.dp, Alignment.Top),
         horizontalAlignment = Alignment.Start,
@@ -138,252 +140,160 @@ fun TutorCard(modifier: Modifier = Modifier) {
                             ),
                             modifier = Modifier.weight(1f)
                         )
-                        Icon(
-                            imageVector = ImageVector.vectorResource(id = R.drawable.heart_icon),
-                            contentDescription = "",
+
+                        LikeButton(
                             modifier = Modifier
                                 .padding(start = 9.dp, top = 5.dp)
-                                .width(21.2.dp)
-                                .height(20.dp)
+                                .width(24.dp)
+                                .height(22.dp),
+                            isLiked = isLiked,
+                            onButtonClicked = { isLiked = !isLiked }
                         )
                     }
                     Row(
                         horizontalArrangement = Arrangement.spacedBy(9.dp, Alignment.Start),
                         verticalAlignment = Alignment.Top,
                     ) {
-                        Row(
-                            horizontalArrangement = Arrangement.spacedBy(7.dp, Alignment.Start),
-                            verticalAlignment = Alignment.CenterVertically,
-                        ) {
-                            Icon(
-                                imageVector = ImageVector.vectorResource(id = R.drawable.star_icon),
-                                tint = StarColor,
-                                contentDescription = null,
-                                modifier = Modifier
-                                    .padding(1.dp)
-                                    .height(15.dp)
-                            )
-                            Text(
-                                text = "4,93",
-                                style = TextStyle(
-                                    fontSize = 14.sp,
-                                    lineHeight = 26.sp,
-                                    fontWeight = FontWeight(500),
-                                    color = Color.Black,
-                                )
-                            )
-                        }
-
-                        Row(
-                            horizontalArrangement = Arrangement.spacedBy(7.dp, Alignment.Start),
-                            verticalAlignment = Alignment.CenterVertically,
-                        ) {
-                            Icon(
-                                imageVector = ImageVector.vectorResource(id = R.drawable.comment_icon),
-                                tint = Color.Black,
-                                contentDescription = null,
-                                modifier = Modifier
-                                    .padding(1.dp)
-                                    .height(15.dp)
-                            )
-                            Text(
-                                text = "100 отзывов",
-                                style = TextStyle(
-                                    fontSize = 14.sp,
-                                    lineHeight = 26.sp,
-                                    fontWeight = FontWeight(500),
-                                    color = Color.Black,
-                                )
-                            )
-                        }
+                        IconWithText(
+                            icon = R.drawable.star_icon,
+                            iconTint = StarColor,
+                            text = "4,93"
+                        )
+                        IconWithText(
+                            icon = R.drawable.comment_icon,
+                            text = "100 отзывов"
+                        )
                     }
-
-
                 }
             }
 
-            Column(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                var seeMore by remember { mutableStateOf(true) }
-
-                Text(
-                    text = "Я являюсь техническим руководителем проекта «Учу на Профи.Ру» и активно использую современные технологии распознавания ",
-                    style = TextStyle(
-                        fontSize = 16.sp,
-                        lineHeight = 24.sp,
-                        fontWeight = FontWeight(400),
-                        color = Color.Black,
-                    ),
-                    maxLines = if (seeMore) 3 else Int.MAX_VALUE,
-                    overflow = TextOverflow.Ellipsis,
-                )
-
-                val textButton = if (seeMore) {
-                    stringResource(id = R.string.see_more)
-                } else {
-                    stringResource(id = R.string.see_less)
-                }
-                Text(
-                    text = textButton,
-                    style = TextStyle(
-                        fontSize = 14.sp,
-                        lineHeight = 24.sp,
-                        fontWeight = FontWeight(400),
-                        color = Color(0xFF3D3D3D),
-                    ),
-                    modifier = Modifier
-                        .clickable {
-                            seeMore = !seeMore
-                        }
-                )
-            }
+            ExpandableText(
+                modifier = modifier.fillMaxWidth(),
+                text = "Я являюсь техническим руководителем проекта «Учу на Профи.Ру» и активно использую современные технологии распознавания "
+            )
 
             Column(
                 verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.Top),
                 horizontalAlignment = Alignment.Start,
                 modifier = Modifier.fillMaxWidth()
             ) {
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(6.dp, Alignment.Top),
-                    horizontalAlignment = Alignment.Start,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(
-                        text = "Предметы",
-                        style = TextStyle(
-                            fontSize = 18.sp,
-                            lineHeight = 24.sp,
-                            fontWeight = FontWeight(500),
-                            color = Color.Black,
-                        )
-                    )
-                    Text(
-                        text = "Математика • Информатика",
-                        style = TextStyle(
-                            fontSize = 16.sp,
-                            lineHeight = 24.sp,
-                            fontWeight = FontWeight(400),
-                            color = Color.Black,
-                        )
-                    )
-                }
+                InfoSection(
+                    modifier = Modifier.fillMaxWidth(),
+                    title = R.string.subjects_section,
+                    body = "Математика • Информатика"
+                )
 
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(6.dp, Alignment.Top),
-                    horizontalAlignment = Alignment.Start,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(
-                        text = "Образование",
-                        style = TextStyle(
-                            fontSize = 18.sp,
-                            lineHeight = 24.sp,
-                            fontWeight = FontWeight(500),
-                            color = Color.Black,
-                        )
-                    )
-                    Text(
-                        text = "Окончил МФТИ, ФОПФ, два красных диплома, 2005 г.",
-                        style = TextStyle(
-                            fontSize = 16.sp,
-                            lineHeight = 24.sp,
-                            fontWeight = FontWeight(400),
-                            color = Color.Black,
-                        )
-                    )
-                }
+                InfoSection(
+                    modifier = Modifier.fillMaxWidth(),
+                    title = R.string.education_section,
+                    body = "Окончил МФТИ, ФОПФ, два красных диплома, 2005 г."
+                )
 
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(6.dp, Alignment.Top),
-                    horizontalAlignment = Alignment.Start,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(
-                        text = "Цены занятий",
-                        style = TextStyle(
-                            fontSize = 18.sp,
-                            lineHeight = 24.sp,
-                            fontWeight = FontWeight(500),
-                            color = Color.Black,
-                        ),
+                PriceSection(
+                    modifier = Modifier.fillMaxWidth(),
+                    title = R.string.prices_section,
+                    subjectsPrices = mapOf(
+                        "Математика" to "500-1000 ₽ / 60 мин",
+                        "Информатика" to "800-1000 ₽ / 60 мин"
                     )
-                    Row(
-                        horizontalArrangement = Arrangement.Start,
-                        verticalAlignment = Alignment.CenterVertically,
-                    ) {
-                        Text(
-                            text = "Математика",
-                            style = TextStyle(
-                                fontSize = 16.sp,
-                                lineHeight = 24.sp,
-                                fontWeight = FontWeight(400),
-                                color = Color.Black,
-                            ),
-                            modifier = Modifier.weight(1f)
-                        )
-                        Text(
-                            text = "500-1000 ₽ / 60 мин",
-                            style = TextStyle(
-                                fontSize = 16.sp,
-                                lineHeight = 24.sp,
-                                fontWeight = FontWeight(400),
-                                color = Color.Black,
-                            ),
-                            textAlign = TextAlign.Right,
-                            modifier = Modifier.weight(1f)
-                        )
-                    }
-                    Row(
-                        horizontalArrangement = Arrangement.Start,
-                        verticalAlignment = Alignment.CenterVertically,
-                    ) {
-                        Text(
-                            text = "Информатика",
-                            style = TextStyle(
-                                fontSize = 16.sp,
-                                lineHeight = 24.sp,
-                                fontWeight = FontWeight(400),
-                                color = Color.Black,
-                            ),
-                            modifier = Modifier.weight(1f)
-                        )
-                        Text(
-                            text = "800-1000 ₽ / 60 мин",
-                            style = TextStyle(
-                                fontSize = 16.sp,
-                                lineHeight = 24.sp,
-                                fontWeight = FontWeight(400),
-                                color = Color.Black,
-                            ),
-                            textAlign = TextAlign.Right,
-                            modifier = Modifier.weight(1f)
-                        )
-                    }
-                }
+                )
             }
         }
 
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(5.dp, Alignment.CenterHorizontally),
-            verticalAlignment = Alignment.CenterVertically,
+        RepetonButton(
             modifier = Modifier
                 .fillMaxWidth()
                 .background(
                     color = Color(0xFF3856BF),
                     shape = RoundedCornerShape(size = 8.dp)
-                )
-                .padding(horizontal = 5.dp, vertical = 10.dp)
-        ) {
-            Text(
-                text = "Написать",
-                style = TextStyle(
-                    fontSize = 14.sp,
-                    lineHeight = 32.sp,
-                    fontWeight = FontWeight(600),
-                    color = Color.White,
-                )
+                ),
+            text = R.string.message_button,
+            textColor = Color.White
+        )
+    }
+}
+
+
+
+@Composable
+fun InfoSection(
+    modifier: Modifier = Modifier,
+    @StringRes title: Int,
+    body: String
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(6.dp, Alignment.Top),
+        horizontalAlignment = Alignment.Start,
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(title),
+            style = TextStyle(
+                fontSize = 18.sp,
+                lineHeight = 24.sp,
+                fontWeight = FontWeight(500),
+                color = Color.Black,
             )
+        )
+        Text(
+            text = body,
+            style = TextStyle(
+                fontSize = 16.sp,
+                lineHeight = 24.sp,
+                fontWeight = FontWeight(400),
+                color = Color.Black,
+            )
+        )
+    }
+}
+
+@Composable
+fun PriceSection(
+    modifier: Modifier = Modifier,
+    @StringRes title: Int,
+    subjectsPrices: Map<String, String>
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(6.dp, Alignment.Top),
+        horizontalAlignment = Alignment.Start,
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(title),
+            style = TextStyle(
+                fontSize = 18.sp,
+                lineHeight = 24.sp,
+                fontWeight = FontWeight(500),
+                color = Color.Black,
+            ),
+        )
+        for (pair in subjectsPrices) {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = pair.key,
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        lineHeight = 24.sp,
+                        fontWeight = FontWeight(400),
+                        color = Color.Black,
+                    ),
+                    modifier = Modifier.weight(1f)
+                )
+                Text(
+                    text = pair.value,
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        lineHeight = 24.sp,
+                        fontWeight = FontWeight(400),
+                        color = Color.Black,
+                    ),
+                    textAlign = TextAlign.Right,
+                    modifier = Modifier.weight(1f)
+                )
+            }
         }
     }
 }

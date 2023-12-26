7b3954b49829d08444846010a441a9987c6edcfb
Nail Shiyatov <nailshiyatov.04@mail.ru>
2023-12-26 14:04:17 +0300

    Refactored ChatsScreen

--- app/src/main/java/com/nailorsh/repeton/ui/screens/ChatsScreen.kt
+++ app/src/main/java/com/nailorsh/repeton/ui/screens/ChatsScreen.kt
@@ -8,11 +8,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.nailorsh.repeton.ui.theme.RepetonTheme
 
 @Composable

dc7390221686c521bea9eef498cd324b56085693
Terminal3D <66626023+Terminal3D@users.noreply.github.com>
2023-12-26 13:16:30 +0200

    lesson screen updated & color file changed

--- app/src/main/java/com/nailorsh/repeton/ui/screens/LessonScreen.kt
+++ app/src/main/java/com/nailorsh/repeton/ui/screens/LessonScreen.kt
@@ -1,64 +1,77 @@
 package com.nailorsh.repeton.ui.screens
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.nailorsh.repeton.R
 import com.nailorsh.repeton.data.FakeLessonSource
 import com.nailorsh.repeton.model.Lesson
+import com.nailorsh.repeton.ui.components.RepetonButton
+import com.nailorsh.repeton.ui.theme.HomeWorkButtonColor
 import com.nailorsh.repeton.ui.theme.RepetonTheme
+import com.nailorsh.repeton.ui.theme.ScreenBackground
+import com.nailorsh.repeton.ui.theme.SubjectColor
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 
 @Composable
 fun LessonScreen(lessonId: Int, modifier: Modifier = Modifier) {
     val lesson = FakeLessonSource.loadLessons()[lessonId]
-
-    Surface(modifier.padding(horizontal = 16.dp, vertical = 32.dp)) {
-        Column {
-            LessonSubject(lesson.subject, Modifier
-                .border(
-                    width = 1.dp,
-                    shape = RoundedCornerShape(8.dp),
-                    color = Color.Black
+    Surface(
+        color = ScreenBackground,
+    ) {
+        Surface(
+            modifier = modifier
+                .padding(horizontal = 16.dp, vertical = 32.dp),
+            color = ScreenBackground,
+            ) {
+            Column {
+                LessonSubject(
+                    lesson.subject, Modifier
+                        .border(
+                            width = 1.dp,
+                            shape = RoundedCornerShape(8.dp),
+                            color = Color.Black
+                        )
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
                 )
-                .padding(horizontal = 16.dp, vertical = 8.dp))
-            LessonCard(lesson, Modifier.padding(vertical = 16.dp))
-            HomeworkCard(lesson.homeworkLink)
-            AdditionalMaterialsCard(lesson.additionalMaterials)
+                LessonCard(lesson, Modifier.padding(bottom = 16.dp))
+                HomeworkCard(lesson.homeworkLink)
+                Spacer(modifier = Modifier.height(32.dp))
+                AdditionalMaterialsCard(lesson.additionalMaterials)
+            }
         }
     }
 }
 
 @Composable
 fun LessonSubject(subject: String, modifier: Modifier = Modifier) {
-    Surface(
+    Box(
         modifier
-
     ) {
         Text(
             text = subject,
             fontSize = 18.sp,
+            lineHeight = 30.sp,
             fontWeight = FontWeight.ExtraBold,
-            color = Color(R.color.subject_color)
+            color = SubjectColor
         )
     }
 }
@@ -66,26 +79,25 @@ fun LessonSubject(subject: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun LessonCard(lesson: Lesson, modifier: Modifier = Modifier) {
-    Card(
+    Box(
         modifier
             .padding(vertical = 16.dp)
-            .background(Color.White)
-        ,
     ) {
-        Column(Modifier
-            .padding(8.dp)
+        Column(
+            Modifier
+                .padding(8.dp)
         ) {
             Text(
                 text = lesson.title,
                 fontSize = 25.sp,
                 fontWeight = FontWeight.Bold,
             )
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(8.dp))
             Text(
                 text = lesson.description.orEmpty(),
                 fontSize = 16.sp,
             )
-            Spacer(modifier = Modifier.height(24.dp))
+            Spacer(modifier = Modifier.height(48.dp))
             TeacherInfo(lesson.teacherName)
             Spacer(modifier = Modifier.height(16.dp))
             TimeInfo(
@@ -101,18 +113,20 @@ fun TeacherInfo(teacherName: String, modifier: Modifier = Modifier) {
     Row {
         Icon(
             painter = painterResource(id = R.drawable.teacher_icon),
-            contentDescription = "Teacher",
+            contentDescription = stringResource(R.string.teacher_icon_desc),
             modifier = modifier
                 .size(24.dp)
                 .padding(start = 4.dp, top = 4.dp)
         )
         Column(modifier = Modifier.padding(start = 8.dp)) {
             Text(
-                text = "Занятие ведёт",
+                text = stringResource(R.string.conduct_lesson),
                 fontSize = 15.sp,
                 fontWeight = FontWeight.Bold,
             )
-            Text(text = teacherName)
+            Text(
+                text = teacherName
+            )
         }
     }
 }
@@ -129,23 +143,90 @@ fun TimeInfo(startTime: String, endTime: String, modifier: Modifier = Modifier) 
 
         )
         Column(modifier = Modifier.padding(start = 8.dp)) {
-            Text(text = "Дата и время занятия")
+            Text(
+                text = stringResource(R.string.date_and_time),
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold
+            )
             Text(text = "$startTime - $endTime")
         }
     }
 }
 
 @Composable
-fun HomeworkCard(hwLink : String?, modifier: Modifier = Modifier) {
-    Card(modifier) {
+fun HomeworkCard(hwLink: String?, modifier: Modifier = Modifier) {
+    Box(modifier) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            Text(
+                text = stringResource(R.string.homework),
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 25.sp
+                )
 
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            RepetonButton(
+
+                buttonColor = HomeWorkButtonColor,
+
+                onClick = {
+                    /* TODO Open Link */
+                }
+            ) {
+                Text(
+                    text = stringResource(R.string.to_homework_button),
+                    style = TextStyle(
+                        fontSize = 15.sp,
+                        lineHeight = 25.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    ),
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                )
+
+            }
+        }
     }
 }
 
 @Composable
 fun AdditionalMaterialsCard(additionalMaterials: String?, modifier: Modifier = Modifier) {
-    Card(modifier) {
+    Box(modifier) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            Text(
+                text = stringResource(R.string.additional_materials),
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 25.sp
+                )
 
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            Box(
+                modifier = Modifier
+                    .background(color = Color.White)
+                    .shadow(elevation = 1.dp)
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp, vertical = 16.dp)
+                    .heightIn(min = 50.dp)
+
+
+            ) {
+                Text(
+                    text = additionalMaterials ?: stringResource(R.string.no_additional_materials),
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        lineHeight = 25.sp,
+                    )
+
+                )
+            }
+
+        }
     }
 }
 
@@ -157,6 +238,6 @@ fun AdditionalMaterialsCard(additionalMaterials: String?, modifier: Modifier = M
 @Composable
 fun LessonScreenPreview() {
     RepetonTheme {
-        LessonScreen(lessonId = 0)
+        LessonScreen(0)
     }
 }
--- app/src/main/java/com/nailorsh/repeton/ui/theme/Color.kt
+++ app/src/main/java/com/nailorsh/repeton/ui/theme/Color.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-val ScreenBackground = Color(android.graphics.Color.parseColor("#EFEFEF"))
+val ScreenBackground = Color(0xFFEFEFEF)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
@@ -24,6 +24,8 @@ val BodyColor = Black
 val ShowMoreTextButtonColor = Color(0xFF3D3D3D)
 val TextFieldTextColor = Black
 val SearchPlaceholderColor = Color(0xFF3D3D3D)
+val HomeWorkButtonColor = Color(0xFF3A65FF)
+val SubjectColor = Color(0xFF3A65FF)
 val BottomNavigationBarColor = Color(0xFFF6F6F6)
 val SelectedBottomBarIconColor = Color(0xFF3856BF)
 val UnselectedBottomBarIconColor = Black

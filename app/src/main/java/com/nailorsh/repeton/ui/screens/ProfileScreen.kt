63416067351ab5da486f98b2626b09676a57c1c2
alexaarkhipova <alexaarkhipova@yandex.ru>
2023-12-26 18:24:48 +0300

    Made LazyColumn for scrollinf

--- app/src/main/java/com/nailorsh/repeton/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/nailorsh/repeton/ui/screens/ProfileScreen.kt
@@ -8,11 +8,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -22,6 +24,7 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAbsoluteAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -40,140 +43,159 @@ import java.nio.file.WatchEvent
 
 @Composable
 fun ProfileScreen() {
-    val scrollState = rememberScrollState()
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = ScreenBackground)
-            .verticalScroll(scrollState)
-    )
-    {
-        Divider(
-            modifier = Modifier
-                .padding(top = 47.6.dp)
-                .width(291.dp)
-                .align(Alignment.CenterHorizontally),
-            color = LineColor,
-            thickness = 1.dp,
+            .background(color = ScreenBackground),
+    ) {
+        item {
+            HorizontalLine()
+        }
 
-            )
-        Row(
-            modifier = Modifier
-                .padding(top = 37.dp)
-                .width(291.dp)
-                .align(Alignment.CenterHorizontally),
-            horizontalArrangement = Arrangement.spacedBy(23.dp, Alignment.Start),
-            verticalAlignment = Alignment.Top,
-        )
-            {
-                val avatarImage = painterResource(R.drawable.peepo)
-                Image(
-                    painter = avatarImage,
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .width(81.dp)
-                        .height(81.dp)
-                        .padding(horizontal = 0.dp)
-                )
-                Column(
-                ) {
-                    Text(
-                        text = "Иван Иванов",
-                        color = Color.Black,
-                        fontSize = 22.sp,
-                        fontWeight = FontWeight.SemiBold
-                        )
-                    Button(
-                        onClick = { },
-                        shape = RoundedCornerShape(size = 5.dp),
-                        modifier = Modifier
-                            .align(Alignment.CenterHorizontally)
-                            .width(151.dp)
-                            .padding(top = 16.dp)
-                            .height(32.dp),
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = AddLessonButtonColor,
-                            contentColor = Color.Black
-                        )
-                    ) {
-                        Text(text = "Настройки",
-                            color = Color.White,
-                            lineHeight = 32.sp,
-                            textAlign = TextAlign.Center
-                        )
-                    }
-                }
-            }
-        Column(
-            modifier = Modifier
-                .width(299.dp)
-                .align(Alignment.CenterHorizontally)
-        )
-        {
+        item {
+            ProfileView()
+        }
+
+        item {
             AboutMeBox("О себе", "Учусь в 11 классе. Планирую поступать в технический вуз, готовлюсь сдавать ЕГЭ по физике и математике")
+        }
+
+        item {
             MyTeachersBox()
+        }
+
+        item {
             MySubjectsBox()
         }
     }
 }
 @Composable
 fun AboutMeBox(title: String, information: String) {
-    Box(
-        modifier = Modifier
-            .padding(top = 53.dp)
-            .width(299.dp)
-            .background(color = Color.White, shape = RoundedCornerShape(size = 5.dp))
-    )
-    {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(12.dp),
+    Column(modifier = Modifier
+        .padding(horizontal = 50.dp)) {
+        Box(
             modifier = Modifier
-                .padding(top = 16.dp, bottom = 16.dp)
+                .padding(top = 53.dp)
+                .width(299.dp)
+                .background(color = Color.White, shape = RoundedCornerShape(size = 5.dp))
         )
         {
-            Text(
-                text = title,
-                color = Color.Black,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(horizontal = 40.dp)
+            Column(
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                modifier = Modifier
+                    .padding(top = 16.dp, bottom = 16.dp)
             )
-            Text(
-                text = information,
-                color = Color.Black,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Normal,
-                modifier = Modifier.padding(horizontal = 40.dp),
-            )
+            {
+                Text(
+                    text = title,
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(horizontal = 40.dp)
+                )
+                Text(
+                    text = information,
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier.padding(horizontal = 40.dp),
+                )
+            }
         }
     }
 }
-
+@Composable
+fun ProfileView() {
+    Column(modifier = Modifier
+    .padding(horizontal = 50.dp)){
+        Row(
+            modifier = Modifier
+                .padding(top = 37.dp),
+            horizontalArrangement = Arrangement.spacedBy(23.dp, Alignment.Start),
+            verticalAlignment = Alignment.Top,
+        ) {
+            val avatarImage = painterResource(R.drawable.peepo)
+            Image(
+                painter = avatarImage,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .width(81.dp)
+                    .height(81.dp)
+                    .padding(horizontal = 0.dp)
+            )
+            Column() {
+                Text(
+                    text = "Иван Иванов",
+                    color = Color.Black,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Button(
+                    onClick = { },
+                    shape = RoundedCornerShape(size = 5.dp),
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .width(151.dp)
+                        .padding(top = 16.dp)
+                        .height(32.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = AddLessonButtonColor,
+                        contentColor = Color.Black
+                    )
+                ) {
+                    Text(
+                        text = "Настройки",
+                        color = Color.White,
+                        lineHeight = 32.sp,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+    }
+}
+@Composable
+fun HorizontalLine() {
+    Column(modifier = Modifier.
+    fillMaxSize()) {
+        Divider(
+            modifier = Modifier
+                .padding(top = 47.6.dp)
+                .align(Alignment.CenterHorizontally)
+                .width(291.dp),
+            color = LineColor,
+            thickness = 1.dp,
+        )
+    }
+}
 @Composable
 fun MyTeachersBox() {
-    Box(
-        modifier = Modifier
-            .padding(top = 30.dp)
-            .width(299.dp)
-            .background(color = Color.White, shape = RoundedCornerShape(size = 5.dp))
-    )
-    {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(12.dp),
+    Column(modifier = Modifier
+        .padding(horizontal = 50.dp)) {
+        Box(
             modifier = Modifier
-                .padding(top = 16.dp, bottom = 16.dp)
+                .padding(top = 30.dp)
+                .width(299.dp)
+                .background(color = Color.White, shape = RoundedCornerShape(size = 5.dp))
         )
         {
-            Text(
-                text = "Мои репетиторы",
-                color = Color.Black,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(horizontal = 40.dp)
+            Column(
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                modifier = Modifier
+                    .padding(top = 16.dp, bottom = 16.dp)
             )
-            MyTeacher("Иванова Мария Ивановна")
-            MyTeacher("Коновалов Александр Владимирович")
+            {
+                Text(
+                    text = "Мои репетиторы",
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(horizontal = 40.dp)
+                )
+                MyTeacher("Иванова Мария Ивановна")
+                MyTeacher("Коновалов Александр Владимирович")
+            }
         }
     }
 }
@@ -202,28 +224,31 @@ fun MyTeacher(teacherName: String) {
 
 @Composable
 fun MySubjectsBox() {
-    Box(
-        modifier = Modifier
-            .padding(top = 30.dp)
-            .width(299.dp)
-            .background(color = Color.White, shape = RoundedCornerShape(size = 5.dp))
-    )
-    {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(12.dp),
+    Column(modifier = Modifier
+        .padding(horizontal = 50.dp)) {
+        Box(
             modifier = Modifier
-                .padding(top = 16.dp, bottom = 16.dp)
+                .padding(top = 30.dp)
+                .width(299.dp)
+                .background(color = Color.White, shape = RoundedCornerShape(size = 5.dp))
         )
         {
-            Text(
-                text = "Мои предметы",
-                color = Color.Black,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(horizontal = 40.dp)
+            Column(
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                modifier = Modifier
+                    .padding(top = 16.dp, bottom = 16.dp)
             )
-            MySubject("Математика")
-            MySubject("Физика")
+            {
+                Text(
+                    text = "Мои предметы",
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(horizontal = 40.dp)
+                )
+                MySubject("Математика")
+                MySubject("Физика")
+            }
         }
     }
 }

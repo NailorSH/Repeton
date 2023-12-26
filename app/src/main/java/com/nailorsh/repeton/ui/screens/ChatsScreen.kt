251a1a07e70262b697aa84a6a0ea97c297387260
Nail Shiyatov <nailshiyatov.04@mail.ru>
2023-12-26 17:22:53 +0300

    Added PlugScreen component

--- app/src/main/java/com/nailorsh/repeton/ui/screens/ChatsScreen.kt
+++ app/src/main/java/com/nailorsh/repeton/ui/screens/ChatsScreen.kt
@@ -1,31 +1,12 @@
 package com.nailorsh.repeton.ui.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.nailorsh.repeton.ui.theme.RepetonTheme
 
 @Composable
 fun ChatsScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = "Этот экран находится в разработке",
-            style = MaterialTheme.typography.headlineMedium,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(16.dp)
-        )
-    }
+
 }
 
 

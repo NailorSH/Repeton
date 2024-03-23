1228c4b452c45740743a14c7accc63b09ef2829f
Nail Shiyatov <nailshiyatov.04@mail.ru>
2024-03-23 17:13:50 +0300

    Fixed a bug with the display of the BottomBar on the login screen and inconsistent behavior with back button navigation on home screen (Issue #5)

--- app/src/main/java/com/nailorsh/repeton/ui/RepetonApp.kt
+++ app/src/main/java/com/nailorsh/repeton/ui/RepetonApp.kt
@@ -4,15 +4,19 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.nailorsh.repeton.domain.AuthViewModel
 import com.nailorsh.repeton.domain.RepetonViewModel
+import com.nailorsh.repeton.domain.Response
 import com.nailorsh.repeton.ui.navigation.AppSections
 import com.nailorsh.repeton.ui.navigation.NavGraph
 import com.nailorsh.repeton.ui.navigation.RepetonBottomBar
+import com.nailorsh.repeton.ui.screens.auth.PhoneLoginUI
 import com.nailorsh.repeton.ui.theme.RepetonTheme
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -23,27 +27,33 @@ fun RepetonApp(
     authViewModel: AuthViewModel = viewModel()
 ) {
     RepetonTheme {
-        Scaffold(
-            bottomBar = {
-                RepetonBottomBar(
-                    tabs = listOf(
-                        AppSections.SEARCH,
-                        AppSections.HOME,
-                        AppSections.CHATS,
-                        AppSections.PROFILE
-                    ).toTypedArray(),
+        val authUiState by authViewModel.signUpState.collectAsState()
+        if (authUiState is Response.Success) {
+            Scaffold(
+                bottomBar = {
+                    RepetonBottomBar(
+                        tabs = listOf(
+                            AppSections.SEARCH,
+                            AppSections.HOME,
+                            AppSections.CHATS,
+                            AppSections.PROFILE
+                        ).toTypedArray(),
 
-                    navController = navController
+                        navController = navController
+                    )
+                }
+            ) { innerPaddingModifier ->
+                NavGraph(
+                    navHostController = navController,
+                    repetonViewModel = repetonViewModel,
+                    modifier = Modifier.padding(innerPaddingModifier)
                 )
             }
-        ) { innerPaddingModifier ->
-            NavGraph(
-                navHostController = navController,
-                repetonViewModel = repetonViewModel,
-                authViewModel = authViewModel,
-                modifier = Modifier.padding(innerPaddingModifier)
+        } else {
+            PhoneLoginUI(
+                viewModel = authViewModel,
+                popUpScreen = {}
             )
         }
     }
-
 }
--- app/src/main/java/com/nailorsh/repeton/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/nailorsh/repeton/ui/navigation/NavGraph.kt
@@ -1,51 +1,28 @@
 package com.nailorsh.repeton.ui.navigation
 
-//import com.nailorsh.repeton.domain.RepetonViewModel
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.nailorsh.repeton.domain.AuthViewModel
 import com.nailorsh.repeton.domain.RepetonViewModel
 import com.nailorsh.repeton.ui.screens.ChatsScreen
 import com.nailorsh.repeton.ui.screens.LessonScreen
 import com.nailorsh.repeton.ui.screens.ProfileScreen
 import com.nailorsh.repeton.ui.screens.ScheduleScreen
 import com.nailorsh.repeton.ui.screens.SearchScreen
-import com.nailorsh.repeton.ui.screens.auth.PhoneLoginUI
 
 @Composable
 fun NavGraph(
     navHostController: NavHostController,
     repetonViewModel: RepetonViewModel,
     modifier: Modifier = Modifier,
-    authViewModel: AuthViewModel
 ) {
     NavHost(
         navController = navHostController,
-        startDestination = AppSections.AUTH.route,
+        startDestination = AppSections.HOME.route,
         modifier = modifier
     ) {
-        composable(AppSections.AUTH.route) {
-            PhoneLoginUI(
-                viewModel = authViewModel,
-
-                /*
-                    TODO исправить баг с кнопкой назад
-                 */
-                popUpScreen = {
-                    val route = AppSections.HOME.route
-
-                    navHostController.popBackStack(
-                        route = route,
-                        inclusive = false,
-                    )
-
-                    navHostController.navigate(route)
-                }
-            )
-        }
         composable(AppSections.SEARCH.route) {
             SearchScreen(
                 getSearchResults = repetonViewModel::getTutors,

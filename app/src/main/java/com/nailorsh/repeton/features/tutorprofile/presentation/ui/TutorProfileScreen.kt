a54048bd0fc346a1b0c02a6ea662f13eec1c0a58
Nail Shiyatov <nailshiyatov.04@mail.ru>
2024-04-19 15:02:30 +0300

    Moved the WorkScheduleButton and AllSubjectsButton buttons into separate composable functions

--- app/src/main/java/com/nailorsh/repeton/features/tutorprofile/presentation/ui/TutorProfileScreen.kt
+++ app/src/main/java/com/nailorsh/repeton/features/tutorprofile/presentation/ui/TutorProfileScreen.kt
@@ -8,21 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,6 +28,7 @@ import com.nailorsh.repeton.core.ui.components.LoadingScreen
 import com.nailorsh.repeton.core.ui.components.TitleWithExpandableText
 import com.nailorsh.repeton.core.ui.theme.RepetonTheme
 import com.nailorsh.repeton.features.tutorprofile.presentation.ui.components.Advantages
+import com.nailorsh.repeton.features.tutorprofile.presentation.ui.components.AllSubjectsButton
 import com.nailorsh.repeton.features.tutorprofile.presentation.ui.components.BottomTutorProfileBar
 import com.nailorsh.repeton.features.tutorprofile.presentation.ui.components.ExperienceYearsNumber
 import com.nailorsh.repeton.features.tutorprofile.presentation.ui.components.LanguageSkills
@@ -44,6 +38,7 @@ import com.nailorsh.repeton.features.tutorprofile.presentation.ui.components.Rat
 import com.nailorsh.repeton.features.tutorprofile.presentation.ui.components.ReviewsNumber
 import com.nailorsh.repeton.features.tutorprofile.presentation.ui.components.TaughtLessonsNumber
 import com.nailorsh.repeton.features.tutorprofile.presentation.ui.components.TopTutorProfileBar
+import com.nailorsh.repeton.features.tutorprofile.presentation.ui.components.WorkScheduleButton
 import com.nailorsh.repeton.features.tutorprofile.presentation.viewmodel.TutorProfileUiState
 import com.nailorsh.repeton.features.tutorprofile.presentation.viewmodel.TutorProfileViewModel
 
@@ -165,39 +160,13 @@ fun TutorProfile(
                 modifier = Modifier.padding(vertical = 20.dp)
             )
 
-            OutlinedButton(
-                onClick = { /*TODO*/ },
-                shape = MaterialTheme.shapes.extraLarge,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = dimensionResource(R.dimen.padding_medium))
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.outline_date_range_24),
-                    contentDescription = null
-                )
-                Spacer(modifier = Modifier.width(dimensionResource(R.dimen.padding_small)))
-                Text(
-                    text = stringResource(R.string.my_work_schedule),
-                    style = MaterialTheme.typography.titleMedium
-                )
-            }
+            WorkScheduleButton {/*TODO*/ }
 
             HorizontalDivider(
                 modifier = Modifier.padding(vertical = dimensionResource(R.dimen.padding_medium))
             )
 
-            TextButton(
-                onClick = { /*TODO*/ },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = dimensionResource(R.dimen.padding_medium))
-            ) {
-                Text(
-                    text = stringResource(R.string.other_subjects),
-                    style = MaterialTheme.typography.titleMedium
-                )
-            }
+            AllSubjectsButton {/*TODO*/ }
 
             Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_medium)))
         }

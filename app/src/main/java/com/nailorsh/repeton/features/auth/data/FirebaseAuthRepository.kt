97c17c18bd9080ae1129129ea99dd7563da22424
Nail Shiyatov <nailshiyatov.04@mail.ru>
2024-05-21 23:07:10 +0300

    Removed authorization via VK ID

--- app/src/main/java/com/nailorsh/repeton/features/auth/data/FirebaseAuthRepository.kt
+++ app/src/main/java/com/nailorsh/repeton/features/auth/data/FirebaseAuthRepository.kt
@@ -188,7 +188,7 @@ class FirebaseAuthRepository @Inject constructor(
     override suspend fun register(user: UserData) = withContext(Dispatchers.IO) {
         val currentUid = auth.currentUser?.uid
         val tmpUserData = user.copy(
-            phone = auth.currentUser?.phoneNumber ?: "No number found"
+            phoneNumber = auth.currentUser?.phoneNumber ?: "No number found"
         )
         if (currentUid != null) {
             val newUserRef = db.collection("users").document(currentUid)
--- app/src/main/java/com/nailorsh/repeton/features/auth/data/model/UserData.kt
+++ app/src/main/java/com/nailorsh/repeton/features/auth/data/model/UserData.kt
@@ -3,5 +3,6 @@ package com.nailorsh.repeton.features.auth.data.model
 data class UserData(
     val name: String = "",
     val surname: String = "",
+    val phoneNumber: String = "",
     val canBeTutor: Boolean = false
 )
